ed9a2e1f604a54accc95e6cc157bd6db373987a0
unknown <https://github.com/Eman-Alghrabawy>
2023-08-28 14:37:14 +0300

    animation

--- eman_news_app/lib/Screens/newsdetail.dart
+++ eman_news_app/lib/Screens/newsdetail.dart
@@ -3,27 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class News extends StatelessWidget {
-  Articles newsData;
-  News({required this.newsData});
+class NewsScreen extends StatelessWidget {
+  final Articles newsData;
+
+  NewsScreen({required this.newsData});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Colors.white,
-      // ),
       body: Stack(
         children: [
           SingleChildScrollView(
             child: Column(
               children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.6,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
+                Hero(
+                  tag: 'news_image_${newsData.urlToImage}',
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
                         image: NetworkImage(newsData.urlToImage ?? ''),
-                        fit: BoxFit.fill),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ),
                 ),
               ],
@@ -41,8 +43,9 @@ class News extends StatelessWidget {
                 decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50)),
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  ),
                 ),
                 child: SingleChildScrollView(
                   child: Padding(
@@ -55,7 +58,9 @@ class News extends StatelessWidget {
                         Text(
                           '${newsData.content}',
                           style: GoogleFonts.nunito(
-                              fontSize: 14, fontWeight: FontWeight.w600),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ],
                     ),
@@ -67,84 +72,94 @@ class News extends StatelessWidget {
           SizedBox(
             height: MediaQuery.of(context).size.height,
             width: double.infinity,
-            child: Column(children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.2,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      FloatingActionButton(
-                        onPressed: () {},
-                        backgroundColor: Color.fromARGB(235, 194, 191, 191),
-                        shape: CircleBorder(),
-                        child: Icon(Icons.arrow_back_ios),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.2,
-              ),
-              Center(
-                child: Container(
-                  width: 311,
-                  height: 141,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(235, 194, 191, 191),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(16),
-                    ),
-                  ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.2,
                   child: Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: Column(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.02,
-                        ),
-                        Text(
-                          '${newsData.title}',
-                          style: GoogleFonts.nunito(
-                              fontSize: 14, fontWeight: FontWeight.w600),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.01,
-                        ),
-                        Text(
-                          '${newsData.publishedAt}',
-                          style: GoogleFonts.nunito(
-                              fontSize: 16, fontWeight: FontWeight.w700),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.02,
-                        ),
-                        Text(
-                          '${newsData.author}',
-                          style: GoogleFonts.nunito(
-                              fontSize: 14, fontWeight: FontWeight.w600),
+                        FloatingActionButton(
+                          onPressed: () {},
+                          backgroundColor: Color.fromARGB(235, 194, 191, 191),
+                          shape: CircleBorder(),
+                          child: Icon(Icons.arrow_back_ios),
                         ),
                       ],
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                  child: Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(40),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                ),
+                Center(
                   child: Container(
-                    child: CircleAvatar(
-                      backgroundColor: const Color.fromARGB(255, 239, 104, 94),
-                      child: SvgPicture.asset('images/Group.svg'),
+                    width: 311,
+                    height: 141,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(235, 194, 191, 191),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(16),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                          Text(
+                            '${newsData.title}',
+                            style: GoogleFonts.nunito(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                          Text(
+                            '${newsData.publishedAt}',
+                            style: GoogleFonts.nunito(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                          Text(
+                            '${newsData.author}',
+                            style: GoogleFonts.nunito(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ))
-            ]),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Container(
+                        child: CircleAvatar(
+                          backgroundColor:
+                              const Color.fromARGB(255, 239, 104, 94),
+                          child: SvgPicture.asset('images/Group.svg'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
